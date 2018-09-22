9ec509ff8833b2f0fd97b34870ac9bd21fd21f92
beljaars <arno.beljaars@ing.com>
2018-09-22 22:34:56 +0200

    First delegate test

--- Program.cs
+++ Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace myApp
 {
     class Program
     {
+        // Start test delegate:
+        public delegate string HelloName(string name);
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World, start testing Blazor");
+
+            var names = new List<string> { "Arno", "Jacq", "Jen" };
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
+
+            TestDelegate(names);           
         }
+
+        static void TestDelegate(List<string> names)
+        {
+            string firstName, secondName;
+
+            HelloName delInstHelloName = PrintName;
+            firstName = delInstHelloName(names[0]);
+            secondName = delInstHelloName(names[1]);
+
+            Console.WriteLine($"Hallo: {firstName.ToString()} and {secondName.ToString()}");
+        }
+
+        static string PrintName(string name)
+        {
+            return name.ToUpper();
+        }
+
+
     }
 }
+
